0f7951bc0c0a7089c3686f00c300c188594647d4
kalys01 <kalysosmon96.g@gmail.com>
2022-01-14 11:40:34 +0600

    deleted Login.vue file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <TheHeaderNavbar />
+  <router-view/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeaderNavbar from '@/components/TheHeaderNavbar.vue'
+
+export default defineComponent({
+  components: {
+    TheHeaderNavbar
+  }
+
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>

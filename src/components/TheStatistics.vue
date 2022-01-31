286e9d920591c7b926cb03e089b1579a9a6ff1e2
kalys01 <kalysosmon96.g@gmail.com>
2022-01-31 14:52:44 +0600

    created a new component which will be the statistics

---
+++ src/components/TheStatistics.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col justify-center text-center my-20">
+    <h1 class="text-xl font-bold mb-8 uppercase dark:text-white">Статистика</h1>
+    <div class="">
+      <img src="https://c.mql5.com/1/78/linechart.gif" width="600" alt="">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>

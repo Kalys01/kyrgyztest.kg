3dc0ea43adb7280b2fdcbc6d93522c7df73b8dbb
kalys01 <kalysosmon96.g@gmail.com>
2022-01-31 14:45:26 +0600

    the footer has been removed so far

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,9 @@
 <template>
   <component :is="layout">
     <router-view />
-    <div class="flex justify-center items-center w-full h-[50px] bottom-0 bg-gray-900 text-gray-100">
+    <!-- <div class="flex justify-center items-center w-full h-[50px] bottom-0 bg-gray-900 text-gray-100">
       footer
-    </div>
+    </div> -->
   </component>
 </template>
 

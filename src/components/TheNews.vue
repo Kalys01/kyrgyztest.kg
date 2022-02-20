414958ed625a882850c7cd54a963482d873cdcc6
kalys01 <kalysosmon96.g@gmail.com>
2022-02-21 00:49:54 +0600

    added image and gradient for beauty

--- src/components/TheNews.vue
+++ src/components/TheNews.vue
@@ -1,85 +1,155 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
-    <section class="w-10/12">
-      <div class="sticky top-0 bg-white py-3 z-10 shadow-xl shadow-white">
-        <h2 class="group-date uppercase">November 2021</h2>
+  <div class="flex flex-col items-center justify-center max-w-7xl mx-auto px-2 relative">
+    <section class="w-10/12 -mr-32">
+      <div class="sticky top-0 bg-white py-3 z-20 rounded-md shadow-xl shadow-white -ml-32 bg-opacity-70 backdrop-filter backdrop-blur-lg">
+        <h2 class="group-date uppercase">2022</h2>
       </div>
       <div class="timeline">
 
         <div class="relative">
-          <div class="dot"></div>
-          <div class="pl-10 relative">
-            <span class="timedate">13 Nov 2021</span>
-            <h3 class="timeline-title">Event 1</h3>
-            <p class="text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
-            </p>
+          <div class="absolute flex justify-center items-center top-1 -left-3.5 h-6 w-6 bg-white rounded-full border-2 border-white dark:bg-gray-900 dark:border-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+            </svg>
+            <span class="-ml-40 absolute text-gray-300">13 Feb 2022</span>
+          </div>
+          <div class="relative h-56 max-h-80 ml-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+            <div class="w-full transition duration-300 ease-in-out text-gray-400 absolute z-10 h-full hover:text-white">
+              <div class="p-8 w-4/6 h-full">
+                <h3 class="timeline-title">«Кыргызтест» – ОшМУ: Кызматташуунун иштиктүү кадамдары</h3>
+                <p class="text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia! omnis pariatur placeat deserunt a fugit debitis 
+                </p>
+              </div>
+            </div>
+            <div class="w-1/2 z-0 absolute right-0">
+              <img class="" src="http://kyrgyztest.gov.kg/wp-content/uploads/2022/01/ОШМУ_1-520x245.jpeg" alt="">
+            </div>
+            <div class="absolute w-1/2 h-full right-0 bg-gradient-to-r from-black"></div>
           </div>
         </div>
 
         <div class="relative">
-          <div class="dot"></div>
-          <div class="pl-10">
-            <span class="timedate">14 Nov 2021</span>
-            <h3 class="timeline-title">Event 2</h3>
-            <p class="text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
-            </p>
+          <div class="absolute flex justify-center items-center top-1 -left-3.5 h-6 w-6 bg-white rounded-full border-2 border-white dark:bg-gray-900 dark:border-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+            </svg>
+            <span class="-ml-40 absolute text-gray-300">12 Jan 2022</span>
+          </div>
+          <div class="relative h-56 max-h-80 ml-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+            <div class="w-full transition duration-300 ease-in-out text-gray-400 absolute z-10 h-full hover:text-white">
+              <div class="p-8 w-4/6 h-full">
+                <h3 class="timeline-title">2021-ЖЫЛДЫН МЫКТЫ МАМЛЕКЕТТИК МЕКЕМЕСИ</h3>
+                <p class="text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
+                </p>
+              </div>
+            </div>
+            <div class="w-1/2 z-0 absolute right-0">
+              <img class="" src="http://kyrgyztest.gov.kg/wp-content/uploads/2022/01/2022-01-25_10-02-26-1280x720.png" alt="">
+            </div>
+            <div class="absolute w-1/2 h-full right-0 bg-gradient-to-r from-black"></div>
           </div>
         </div>
 
         <div class="relative">
-          <div class="dot"></div>
-          <div class="pl-10">
-            <span class="timedate">15 Nov 2021</span>
-            <h3 class="timeline-title">Event 3</h3>
-            <p class="text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vitae temporibus impedit accusamus architecto? Dolores commodi dignissimos voluptate at nihil!
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vitae temporibus impedit accusamus architecto? Dolores commodi dignissimos voluptate at nihil!
-            </p>
+          <div class="absolute flex justify-center items-center top-1 -left-3.5 h-6 w-6 bg-white rounded-full border-2 border-white dark:bg-gray-900 dark:border-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+            </svg>
+            <span class="-ml-40 absolute text-gray-300">11 Jan 2022</span>
+          </div>
+          <div class="relative h-56 max-h-80 ml-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+            <div class="w-full transition duration-300 ease-in-out text-gray-400 absolute z-10 h-full hover:text-white">
+              <div class="p-8 w-4/6 h-full">
+                <h3 class="timeline-title">«Кыргызтестте» 3 айлык жарыяланды</h3>
+                <p class="text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
+                </p>
+              </div>
+            </div>
+            <div class="w-1/2 z-0 absolute right-0">
+              <img class="" src="http://kyrgyztest.gov.kg/wp-content/uploads/2021/07/3-1080x720.jpg" alt="">
+            </div>
+            <div class="absolute w-1/2 h-full right-0 bg-gradient-to-r from-black"></div>
           </div>
         </div>
 
       </div>
     </section>
 
-    <section class="w-10/12">
-      <div class="sticky top-0 bg-white py-3 z-10 shadow-xl shadow-white  rounded-md dark:bg-gray-600">
-        <h2 class="group-date uppercase">December 2021</h2>
+    <section class="w-10/12 -mr-32">
+      <div class="sticky top-0 bg-white py-3 z-20 rounded-md shadow-xl shadow-white -ml-32 bg-opacity-70 backdrop-filter backdrop-blur-lg">
+        <h2 class="group-date uppercase">2021</h2>
       </div>
       <div class="timeline">
         
         <div class="relative">
-          <div class="dot"></div>
-          <div class="pl-10">
-            <span class="timedate">13 Nov 2021</span>
-            <h3 class="timeline-title">Event 1</h3>
-            <p class="text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
-            </p>
+          <div class="absolute flex justify-center items-center top-1 -left-3.5 h-6 w-6 bg-white rounded-full border-2 border-white dark:bg-gray-900 dark:border-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+            </svg>
+            <span class="-ml-40 absolute text-gray-300">11 Nov 2021</span>
+          </div>
+          <div class="relative h-56 max-h-80 ml-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+            <div class="w-full transition duration-300 ease-in-out text-gray-400 absolute z-10 h-full hover:text-white">
+              <div class="p-8 w-4/6 h-full">
+                <h3 class="timeline-title">«Кыргызтест» ишин улантууда</h3>
+                <p class="text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
+                </p>
+              </div>
+            </div>
+            <div class="w-1/2 z-0 absolute right-0">
+              <img class="" src="http://kyrgyztest.gov.kg/wp-content/uploads/2021/06/4-960x720.jpg" alt="">
+            </div>
+            <div class="absolute w-1/2 h-full right-0 bg-gradient-to-r from-black"></div>
           </div>
         </div>
 
         <div class="relative">
-          <div class="dot"></div>
-          <div class="pl-10">
-            <span class="timedate">14 Nov 2021</span>
-            <h3 class="timeline-title">Event 2</h3>
-            <p class="text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
-            </p>
+          <div class="absolute flex justify-center items-center top-1 -left-3.5 h-6 w-6 bg-white rounded-full border-2 border-white dark:bg-gray-900 dark:border-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+            </svg>
+            <span class="-ml-40 absolute text-gray-300">11 Nov 2021</span>
+          </div>
+          <div class="relative h-56 max-h-80 ml-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+            <div class="w-full transition duration-300 ease-in-out text-gray-400 absolute z-10 h-full hover:text-white">
+              <div class="p-8 w-4/6 h-full">
+                <h3 class="timeline-title">«КЫРГЫЗТЕСТ» ЖАҢЫЛЫКТАРЫ</h3>
+                <p class="text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
+                </p>
+              </div>
+            </div>
+            <div class="w-1/2 z-0 absolute right-0">
+              <img class="" src="http://kyrgyztest.gov.kg/wp-content/uploads/2020/12/IMG_5074-1024x683.jpg" alt="">
+            </div>
+            <div class="absolute w-1/2 h-full right-0 bg-gradient-to-r from-black"></div>
           </div>
         </div>
 
         <div class="relative">
-          <div class="dot"></div>
-          <div class="pl-10">
-            <span class="timedate">15 Nov 2021</span>
-            <h3 class="timeline-title">Event 3</h3>
-            <p class="text-gray-500">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vitae temporibus impedit accusamus architecto? Dolores commodi dignissimos voluptate at nihil!
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vitae temporibus impedit accusamus architecto? Dolores commodi dignissimos voluptate at nihil!
-            </p>
+          <div class="absolute flex justify-center items-center top-1 -left-3.5 h-6 w-6 bg-white rounded-full border-2 border-white dark:bg-gray-900 dark:border-gray-900">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+            </svg>
+            <span class="-ml-40 absolute text-gray-300">11 Nov 2021</span>
+          </div>
+          <div class="relative h-56 max-h-80 ml-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+            <div class="w-full transition duration-300 ease-in-out text-gray-400 absolute z-10 h-full hover:text-white">
+              <div class="p-8 w-4/6 h-full">
+                <h3 class="timeline-title">ТЕСТ ТАПШЫРУУЧУЛАРГА МААНИЛҮҮ МААЛЫМАТ</h3>
+                <p class="text-gray-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab minima ullam hic tempora, impedit illum, sint corporis reiciendis expedita laboriosam doloremque omnis pariatur placeat deserunt a fugit debitis dignissimos officia!
+                </p>
+              </div>
+            </div>
+            <div class="w-1/2 z-0 absolute right-0">
+              <img class="" src="http://kyrgyztest.gov.kg/wp-content/uploads/2021/11/MG_9989-960x720.jpg" alt="">
+            </div>
+            <div class="absolute w-1/2 h-full right-0 bg-gradient-to-r from-black"></div>
           </div>
         </div>
 

2018d0128eccb367393acb50adca83c4653a0d77
kalys01 <kalysosmon96.g@gmail.com>
2022-03-16 17:09:52 +0600

    added a smooth transition

--- src/layouts/AppLayoutNews.vue
+++ src/layouts/AppLayoutNews.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full bg-white dark:bg-gray-900 dark:text-white">
+  <div class="w-full bg-white dark:bg-gray-900 dark:text-white transition duration-500">
     <slot />
   </div>
 </template>

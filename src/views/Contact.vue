82aa219a6bea945559af5ade241be64961d5bea1
kalys01 <kalysosmon96.g@gmail.com>
2022-03-17 12:53:59 +0600

    background done

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="">
-    <h1>This is an contact page</h1>
+  <div class="min-h-screen w-full ">
+    <div class="w-1/2 h-screen bg-gray-900 flex items-center dark:bg-white  transition duration-500">
+      <div class="absolute w-[500px] h-[300px] bg-opacity-0 backdrop-filter backdrop-blur-lg shadow-xl right-0"></div>
+    </div>
+    <div class="w-1/2 absolute right-0 max-h-screen bg-white dark:bg-gray-900 transition duration-500"></div>
+    
   </div>
 </template>
 

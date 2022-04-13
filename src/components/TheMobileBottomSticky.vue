d7a8874ccfa084287a8fa6edc0fe4cdc92d357f0
kalys01 <kalysosmon96.g@gmail.com>
2022-04-13 17:25:52 +0600

    installed Icons and connected

--- src/components/TheMobileBottomSticky.vue
+++ src/components/TheMobileBottomSticky.vue
@@ -1,48 +1,38 @@
 <template>
   <div class="w-full flex items-center h-16 z-50 fixed bottom-0 bg-black/50 rounded-t-md backdrop-filter backdrop-blur-lg shadow-xl">
-    <ul class="w-full flex justify-around text-white">
+    <ul class="w-full flex justify-around text-gray-400">
 
       <router-link to="/">
         <li class="flex flex-col justify-center items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-          </svg>
+          <HomeIcon class="h-6 w-6"/>
           <span class="text-xs">Главная</span>
         </li>
       </router-link>
 
       <router-link to="/news">
         <li class="flex flex-col justify-center items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
-          </svg>
+          <NewspaperIcon class="w-6 h-6"/>
           <span class="text-xs">Новости</span>
         </li>
       </router-link>
 
       <router-link to="/leadership">
         <li class="flex flex-col justify-center items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-          </svg>
+          <UserIcon class="h-6 w-6"/>
           <span class="text-xs">Руководство</span>
         </li>
       </router-link>
 
       <router-link to="/result">
         <li class="flex flex-col justify-center items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-          </svg>
+          <CheckCircleIcon class="w-6 h-6"/>
           <span class="text-xs">Результаты</span>
         </li>
       </router-link>
 
       <router-link to="/contact">
         <li class="flex flex-col justify-center items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
-          </svg>
+          <IdentificationIcon class="h-6 w-6"/>
           <span class="text-xs">Контакты</span>
         </li>
       </router-link>
@@ -53,8 +43,20 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { HomeIcon } from '@heroicons/vue/outline'
+import { NewspaperIcon } from '@heroicons/vue/outline'
+import { UserIcon } from '@heroicons/vue/outline'
+import { IdentificationIcon } from '@heroicons/vue/outline'
+import { CheckCircleIcon } from '@heroicons/vue/outline'
 
 export default defineComponent({
+  components: {
+    UserIcon,
+    HomeIcon,
+    NewspaperIcon,
+    CheckCircleIcon,
+    IdentificationIcon
+  },
   setup() {
       
   },

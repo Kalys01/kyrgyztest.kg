edb5758932286a58920557174fb9c67dc266a49e
kalys01 <kalysosmon96.g@gmail.com>
2022-04-13 12:39:04 +0600

    adaptive footer is ready :)

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center w-full h-[150px] bottom-0 border-t-[1px] border-white mb-12 xs:mb-0 bg-gray-300 text-gray-800 dark:text-gray-300 dark:bg-gray-900 transition duration-500">
+    <p class="text-xs sm:text-sm md:text-xl">Государственное учреждение "КЫРГЫЗТЕСТ" © <span>2022</span>.</p>
+  </div>
+</template>

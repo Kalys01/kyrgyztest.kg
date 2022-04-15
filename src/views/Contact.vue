e24b6d4d3e532d3fe88f0c00670d48808ef40ae3
kalys01 <kalysosmon96.g@gmail.com>
2022-04-15 10:13:12 +0600

    Moved contacts to a separate folder

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center">
-    <div class=" min-h-screen w-full max-w-screen-xl bg-gray-400">
-      <div class="w-full h-full p-10 bg-blue-400 flex justify-end items-center">
+    <div class="w-full max-w-screen-xl bg-gray-400">
+      <div class="w-full px-10 bg-blue-400 flex justify-end items-center py-20">
         <div class="bg-white w-[calc(100%-200px)] right-0 h-[500px] relative rounded-md">
           <ContactsMap />
           <ContactsInfo />
@@ -13,8 +13,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ContactsInfo from '@/components/TheContactsInfo.vue'
-import ContactsMap from '@/components/TheContactsMap.vue'
+import ContactsInfo from '@/components/Contact/TheContactsInfo.vue'
+import ContactsMap from '@/components/Contact/TheContactsMap.vue'
 
 export default defineComponent({
   components: {

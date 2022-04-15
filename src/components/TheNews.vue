e34a861e963408f72e6fcc6c4bbb3cf40ccdfe99
kalys01 <kalysosmon96.g@gmail.com>
2022-04-15 17:20:45 +0600

    removed the sticky block which indicated the year

--- src/components/TheNews.vue
+++ src/components/TheNews.vue
@@ -110,27 +110,7 @@
               <div class="absolute w-full sm:w-1/2 h-1/2 sm:h-full sm:right-0 bg-gradient-to-t from-black sm:bg-gradient-to-r sm:from-black"></div>
             </div>
           </div>
-
-
-          <div class="sticky top-0 z-10 bg-white w-full py-5">
-            <span>Дасыгын жогорулатуу курстары:</span>
-          </div>
-          <div class="py-5">
-            <div class="w-5 h-5 rounded-full absolute -ml-3 bg-black flex justify-center items-center border-2 border-white">
-              <div class="absolute">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
-                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-                </svg>
-              </div>
-              <span class="absolute -ml-36 text-gray-300">13 Dec 2021</span>
-            </div>
-            <div class="mx-5">
-              <h1>2021-ЖЫЛДЫН МЫКТЫ МАМЛЕКЕТТИК МЕКЕМЕСИ</h1>
-              <p>
-                kalys Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto accusantium, nisi vel laborum dolorum facere illo quas voluptate harum exercitationem! psum dolor sit, amet consectetur adipisicing elit. Iusto accusantium, nisi vel laborum dolorum facere illo quas voluptate harum exercitationem!
-              </p>
-            </div>
-          </div>
+          
         </div>
       </div>
 

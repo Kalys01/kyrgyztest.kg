7b66b0472d7460de8310646cb3c16477f1068fe9
kalys01 <kalysosmon96.g@gmail.com>
2022-04-19 17:23:00 +0600

    Declared the vueperslides module because  didn't work imported components

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'vueperslides' {
+  class VueperSlides extends DefineComponent {}
+  class VueperSlide extends DefineComponent {}
+}

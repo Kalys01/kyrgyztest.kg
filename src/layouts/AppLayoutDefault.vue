5dff6f292c284ac4fb225bf73acbe39aee3c1f02
kalys01 <kalysosmon96.g@gmail.com>
2022-04-19 13:00:38 +0600

    Added flex-col class to display elements as a column

--- src/layouts/AppLayoutDefault.vue
+++ src/layouts/AppLayoutDefault.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-300 flex justify-center dark:bg-gray-800">
+  <div class="bg-gray-300 flex flex-col justify-center dark:bg-gray-800">
     <slot />
   </div>
 </template>

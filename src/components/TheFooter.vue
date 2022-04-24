f2c9cc111c07987fd2c314859b937b4e2234f563
kalys01 <kalysosmon96.g@gmail.com>
2022-04-24 22:31:24 +0600

    Added dinamic year

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="flex justify-center items-center w-full h-[150px] bottom-0 border-t-[1px] border-white mb-12 xs:mb-0 bg-gray-300 text-gray-800 dark:text-gray-300 dark:bg-gray-900 transition duration-500">
-    <p class="text-xs sm:text-sm md:text-xl">Государственное учреждение "КЫРГЫЗТЕСТ" © <span>2022</span>.</p>
+    <p class="text-xs sm:text-sm md:text-xl">Государственное учреждение "КЫРГЫЗТЕСТ" © <span>{{ currentYear }}</span>.</p>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const currentYear = ref<number>(new Date().getFullYear());
+</script>

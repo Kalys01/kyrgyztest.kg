03a8279e2c4baebbad3590cd93f685bde9eaf652
kalys01 <kalysosmon96.g@gmail.com>
2022-05-05 19:03:21 +0600

    Added picture and pdf file

--- src/views/Leadership.vue
+++ src/views/Leadership.vue
@@ -3,44 +3,19 @@
     <div class="w-full lg:w-11/12 max-w-screen-xl">
 
       <div class="grid grid-cols-12">
-        <div class="col-span-1 sm:col-span-3 bg-gray-900 dark:bg-gray-100"></div>
-        <div class="col-span-11 sm:col-span-9 border-l-2 border-gray-300 bg-blue-300 py-5">
-          <div class="sticky top-0 z-10 bg-white w-full py-5">
-            <span>Билими:</span>
+        <div class="xs:col-span-3 bg-[#292828]"></div>
+        <div class="flex flex-col items-center justify-center col-span-12 xs:col-span-9 border-l-2 border-gray-300 bg-gray-300 pt-16">
+          <div class="w-[310px] overflow-hidden">
+            <img src="https://kyrgyztest.gov.kg/wp-content/uploads/2021/04/44444-548x720.jpg" alt="">
           </div>
-          <div class="py-5">
-            <div class="w-5 h-5 rounded-full absolute -ml-3 bg-black flex justify-center items-center border-2 border-white">
-              <div class="absolute">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
-                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-                </svg>
-              </div>
-              <span class="absolute -ml-36 text-gray-300">2012-2014-жж.</span>
-            </div>
-            <div class="mx-5">
-              Россия эл чарбасы жана Мамлекеттик кызмат академиясынын базасындагы (РАНХ) Улуу Британиянын Манчестер университетинин магистратурасы, магистр диплому; Москва Социалдык жана экономикалык илимдердин жогорку мектеби (МВШСЭН), «Билим берүүдөгү башкаруу/билим берүү системасынын сапатын баалоо менеджменти» факультети, – кайрадан даярдоо диплому;
-            </div>
-          </div>
-          <div class="sticky top-0 z-10 bg-white w-full py-5">
-            <span>Дасыгын жогорулатуу курстары:</span>
-          </div>
-          <div class="py-5">
-            <div class="w-5 h-5 rounded-full absolute -ml-3 bg-black flex justify-center items-center border-2 border-white">
-              <div class="absolute">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300 " viewBox="0 0 20 20" fill="currentColor">
-                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-                </svg>
-              </div>
-              <span class="absolute -ml-36 text-gray-300">2012-2014-жж.</span>
-            </div>
-            <div class="mx-5">
-              kalys Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto accusantium, nisi vel laborum dolorum facere illo quas voluptate harum exercitationem! psum dolor sit, amet consectetur adipisicing elit. Iusto accusantium, nisi vel laborum dolorum facere illo quas voluptate harum exercitationem!
-            </div>
+          <div class="flex flex-col justify-center gap-7 items-center w-full text-xl font-semibold text-[#3a3838] uppercase my-10">
+            <h1 class="text-sm md:text-lg">Топоева-Ставинская Батма Болоталиевна</h1>
+            <button class="hover:text-[#673AB7] transition duration-200 border-[1px] border-[#673AB7] rounded-3xl py-2 px-7">
+              <a class="text-md md:text-2xl" href="https://kyrgyztest.gov.kg/wp-content/uploads/2020/12/Топоева-Ставинская_ТАРЖЫМАЛ_кырг.pdf">Директор</a>
+            </button>
           </div>
         </div>
       </div>
-
-      <span>Дасыгын жогорулатуу курстары:</span>
     </div>
   </div>
 </template>

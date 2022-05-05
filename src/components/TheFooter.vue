8a7caa7c5635589716d14bd39f152933c8819666
kalys01 <kalysosmon96.g@gmail.com>
2022-05-05 16:23:08 +0600

    Added a black block at the end

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex justify-center items-center w-full h-[150px] bottom-0 border-t-[1px] border-white mb-12 xs:mb-0 bg-gray-300 text-gray-800 dark:text-gray-300 dark:bg-gray-900 transition duration-500">
+  <div class="flex justify-center items-center w-full h-[100px] bottom-0 border-t-[1px] border-white bg-gray-300 text-gray-800 dark:text-gray-300 dark:bg-gray-900 transition duration-500">
     <p class="text-xs sm:text-sm md:text-xl">Государственное учреждение "КЫРГЫЗТЕСТ" © <span>{{ currentYear }}</span>.</p>
   </div>
+  <div class="bg-black h-16 rounded-t-md w-full xs:hidden"></div>
 </template>
 
 <script lang="ts" setup>

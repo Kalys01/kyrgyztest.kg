2988fa36cf31cc0b211c9f0c0378feddbe6bf233
kalys01 <kalysosmon96.g@gmail.com>
2022-05-26 01:17:06 +0600

    Created a new folder and placed the TheStatistics.vue and Bar Chart.ts component there

---
+++ src/components/Statistics/BarChart.ts
@@ -0,0 +1,85 @@
+import { defineComponent, h, PropType } from 'vue'
+
+import { Bar } from 'vue-chartjs'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Plugin
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default defineComponent({
+  name: 'BarChart',
+  components: {
+    Bar
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object as PropType<Partial<CSSStyleDeclaration>>,
+      default: () => {
+     
+      }
+    },
+    plugins: {
+      type: Array as PropType<Plugin<'bar'>[]>,
+      default: () => []
+    }
+  },
+  setup(props) {
+    const chartData = {
+      labels: [
+        '2014-2017',
+        '2018',
+        '2019',
+        '2020'
+      ],
+      datasets: [
+        {
+          label: 'Количество тестируемых',
+          backgroundColor: '#673AB7',
+          data: [10000, 19319, 18039, 5860]
+        }
+      ]
+    }
+
+    const chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+
+    return () =>
+      h(Bar, {
+        chartData,
+        chartOptions,
+        chartId: props.chartId,
+        width: props.width,
+        height: props.height,
+        cssClasses: props.cssClasses,
+        styles: props.styles,
+        plugins: props.plugins
+      })
+  }
+})
--- src/components/Statistics/TheStatistics.vue
+++ src/components/Statistics/TheStatistics.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="flex flex-col justify-center text-center my-20">
     <h1 class="text-xl font-bold mb-8 uppercase dark:text-white">Статистика</h1>
-    <div class="">
-      <img src="https://c.mql5.com/1/78/linechart.gif" width="600" alt="">
+    <div class=" dark:bg-gray-900">
+      <BarChart />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import BarChart from './BarChart'
 
 export default defineComponent({
+  components: {
+    BarChart
+  }
 
 })
 </script>

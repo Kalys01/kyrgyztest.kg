0d9201006e4a0cdadd446ca7f31e4912e5c08876
kalys01 <kalysosmon96.g@gmail.com>
2022-06-20 23:44:11 +0600

    Changed the style

--- src/views/Leadership.vue
+++ src/views/Leadership.vue
@@ -2,17 +2,37 @@
   <div class="grid grid-cols-12">
     <div class="xs:col-span-3 bg-[#292828]"></div>
     <div class="flex flex-col items-center justify-center col-span-12 xs:col-span-9 border-l-2 border-gray-300 bg-gray-300 pt-16">
-      <div class="w-[310px] overflow-hidden">
-        <img src="https://kyrgyztest.gov.kg/wp-content/uploads/2021/04/44444-548x720.jpg" alt="">
-      </div>
-      <div class="flex flex-col justify-center gap-7 items-center w-full text-xl font-semibold text-[#3a3838] uppercase my-10">
-        <h1 class="text-sm md:text-lg">Топоева-Ставинская Батма Болоталиевна</h1>
-        <a
-          class="hover:text-[#673AB7] transition duration-200 border-[1px] border-[#673AB7] rounded-3xl py-2 px-7 text-sm md:text-md"
-          href="https://kyrgyztest.gov.kg/wp-content/uploads/2020/12/Топоева-Ставинская_ТАРЖЫМАЛ_кырг.pdf"
-        >
-          Директор
-        </a>
+      <div class="w-[355px]  flex flex-col bg-white rounded-xl items-center px-4 shadow-lg relative my-16">
+        <div class="w-[290px] rounded-xl overflow-hidden -top-14 absolute shadow-lg">
+          <img src="https://kyrgyztest.gov.kg/wp-content/uploads/2021/04/44444-548x720.jpg" alt="">
+        </div>
+        <div class="w-full mt-[340px] mb-6 flex flex-col items-center text-[#292828]">
+          <div class="flex flex-col text-center">
+            <h1 class="text-md font-thin">Топоева-Ставинская Батма Болоталиевна</h1>
+            <span class="text-xs text-gray-400 font-mono px-3">Директор  Государственной учереждении Кыргызтест</span>
+          </div>
+          <div class="w-full flex my-5">
+
+            <div class="flex flex-col text-center w-1/2">
+              <h1 class="text-md">+44 года</h1>
+              <span class="text-xs text-gray-400 font-mono">Общий стаж работы</span>
+            </div>
+            <div class="flex flex-col text-center w-1/2">
+               <h1 class="text-md">26 лет</h1>
+              <span class="text-xs text-gray-400 font-mono">Педагогический стаж</span>
+            </div>
+
+          </div>
+          <div class="flex">
+            <a
+              class="hover:text-[#673AB7] transition duration-200 border-[1px] border-[#673AB7] rounded-full py-2 px-7 text-sm"
+              href="https://kyrgyztest.gov.kg/wp-content/uploads/2020/12/РЕЗЮМЕ_Топоевой-Ставинской_рус_.pdf"
+            >
+              Подробнее
+            </a>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>

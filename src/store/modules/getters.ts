c96e7ce47fffb5d6c83982a0101200a5faffc0c9
kalys01 <kalysosmon96.g@gmail.com>
2022-08-15 10:42:23 +0600

    Transferred data from the component to the store

--- src/store/modules/getters.ts
+++ src/store/modules/getters.ts
@@ -1,11 +1,16 @@
+import  NavUp  from '@/models/ModelNavUp';
 import { GetterTree } from "vuex";
 import { State } from './state';
 
 export type Getters = {
-  GET_THEME(state: State): string | boolean
+  GET_THEME(state: State): string 
+  GET_NAVUPS(state: State): NavUp[]
 }
 export const getters: GetterTree<State, State> & Getters = {
   GET_THEME(state) {
     return state.theme;
+  },
+  GET_NAVUPS(state) {
+    return state.navUps;
   }
 }
--- src/store/modules/state.ts
+++ src/store/modules/state.ts
@@ -1,7 +1,18 @@
+import NavUp from "@/models/ModelNavUp";
+
 export type State = {
   theme: string;
+  navUps: NavUp[];
 };
 
 export const state: State = {
   theme: 'light',
+  navUps: [
+    {title: 'Нормативные акты', to: '/normative'},
+    {title: 'Выпускникам школ', to: '/forschool'},
+    {title: 'Система Кыргызтест', to: '/system'},
+    {title: 'График проведения теста', to: '/graph'},
+    {title: 'Тренировочные тесты', to: '/training'},
+    {title: 'Объявления', to: '/ads'}
+  ]
 };

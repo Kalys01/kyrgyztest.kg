698b21e59b27086e57dfe2a35e12d759389052c3
kalys01 <kalysosmon96.g@gmail.com>
2022-10-19 21:49:58 +0600

    Installed i18n for multilanguage

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store } from './store'
+import { createI18n } from 'vue-i18n'
 import './assets/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const i18n = createI18n({
+    locale: 'ja',
+  fallbackLocale: 'en'
+  })
+
+createApp(App).use(i18n).use(store).use(router).mount('#app')

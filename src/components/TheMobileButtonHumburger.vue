070cfd8675e9e68834ed04e766d20609c3098ebd
kalys01 <kalysosmon96.g@gmail.com>
2022-10-19 15:23:29 +0600

    Changed the menu Icon

--- src/components/TheMobileButtonHumburger.vue
+++ src/components/TheMobileButtonHumburger.vue
@@ -1,37 +1,20 @@
 <template>
-  <button class="cursor-pointer border-2 border-white rounded-md" @click="openSidebar">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-5 w-5"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M4 6h16M4 12h16M4 18h7"
-      />
-    </svg>
+  <button
+    class="cursor-pointer"
+    @click="openSidebar"
+  >
+    <MenuIcon class="w-6 h-6"/>
   </button>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { MutationTypes } from "@/store/modules/mutation-types";
-import { defineComponent } from "vue";
 import { useStore } from "@/store";
+import { MenuIcon } from '@heroicons/vue/outline'
 
-export default defineComponent({
-  name: "openSidebar",
-  setup() {
-    const store = useStore();
+const store = useStore();
 
-    const openSidebar = () => {
-      store.commit(MutationTypes.SET_SHOWSIDEBAR, true);
-    };
-
-    return { openSidebar };
-  },
-});
+const openSidebar = () => {
+  store.commit(MutationTypes.SET_SHOWSIDEBAR, true);
+};
 </script>

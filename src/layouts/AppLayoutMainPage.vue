7f1aa5008e6939bc615fa4fc58eb66b0ac31bf75
kalys01 <kalysosmon96.g@gmail.com>
2023-01-30 20:39:04 +0600

    Created new layouts and applied them to the main page and on the news page

---
+++ src/layouts/AppLayoutMainPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full flex flex-col bg-white dark:bg-gray-900 dark:text-white transition duration-500">
+      <slot />
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>

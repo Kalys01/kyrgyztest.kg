ed24fc35ce139a316a135404cfbaf72481183e48
kalys01 <kalysosmon96.g@gmail.com>
2023-02-03 13:20:01 +0600

    Created a separate component for the TheGraphic.vue and ForSchool.vue and brought it to the main page

---
+++ src/components/TheForSchool.vue
@@ -0,0 +1,35 @@
+<template>
+    <TheBlockComponent 
+      title="Выпускникам школ"
+      :backgroundColor="'bg-gray-500'"
+    >
+      <div class="flex justify-center gap-6">
+        <div class="flex flex-col justify-center items-center w-full max-w-[800px] tracking-wider">
+          <div class="text-md">
+            <p class="mb-4 leading-7">
+              Согласно Постановлению Правительства Кыргызской Республики №279 от 11 июня 2018 года Государственное учреждение «Кыргызтест» тестирование по государственному языку для выпускников общеобразовательных учреждений будет проводиться с 20 апреля по 15 июля 2021 года.
+            </p>  
+            <h2 class="font-bold">При себе необходимо иметь:</h2>
+            <p class="flex flex-col pl-10 my-3 leading-8">
+              <span>документ, удостоверяющий личность (паспорт, свидетельство о рождении)</span>
+              <span>справку с места учебы, заверенную печатью и подписью.</span>
+            </p>
+            <h2 class="font-bold">Уровень для выпускников школ:</h2>
+            <p class="flex flex-col pl-10 my-3 leading-8">
+              <span>— с обучением на кыргызском языке – В1;</span>
+              <span>— с обучением на русском языке – А2.</span>
+              <span>Пересдача теста не допускается.</span>
+            </p>
+          </div>
+        </div>
+        <div class="overflow-hidden max-w-xl">
+          <img src="@/assets/img/Pupils.jpg" class="rounded-md" alt="school">
+        </div>
+      </div>
+    </TheBlockComponent>
+</template>
+
+<script lang="ts" setup>
+import TheBlockComponent from "@/components/TheBlockComponent.vue";
+
+</script>
---
+++ src/components/TheGraphic.vue
@@ -0,0 +1,30 @@
+<template>
+    <TheBlockComponent
+      title="График проведения теста"
+      :backgroundColor="'bg-yellow-500'"
+    >
+      <div class="flex justify-center gap-6">
+        <div class="flex flex-col justify-center items-center w-full max-w-[800px] tracking-wider">
+          <div class="flex flex-col justify-center w-full max-w-[800px] tracking-wider text-md leading-8">
+            <p class="mb-4 leading-7">
+              «Кыргызтест» мамлекеттик мекемеси саат <span class="font-bold">9:00</span>дон <span class="font-bold">18:00</span>го чейин иштейт, түшкү тыныгуу саат <span class="font-bold">12:00</span>дон <span class="font-bold">13:00</span>го чейин болот.
+            </p>
+            <h2 class="font-bold">Тест алдын ала бекитилген график боюнча эки нөөмөттө жүргүзүлөт.</h2>
+            <p class="flex flex-col pl-10 my-3 ">
+              <span>— Биринчи нөөмөткө каттоо саат 9:00до башталат</span>
+              <span>— Экинчи нөөмөткө каттоо саат 13:30да башталат</span>
+            </p>
+            <p>Тест акысы төлөнбөгөн учурда тестке катышууга уруксат берилбейт (КР 2014-жылдын 17-июлундагы №139 «Мамлекеттик жана муниципалдык кызмат көрсөтүүлөр жөнүндө» Мыйзамынын 2-главасынын 7-беренесинин 1, 2-пункттарынан тышкары)</p>
+          </div>
+        </div>
+        <div class=" overflow-hidden  max-w-xl">
+          <img src="@/assets/img/WorkGraphic.jpg" class="rounded-md" alt="school">
+        </div>
+      </div>
+    </TheBlockComponent>
+</template>
+
+<script lang="ts" setup>
+import TheBlockComponent from "@/components/TheBlockComponent.vue";
+
+</script>

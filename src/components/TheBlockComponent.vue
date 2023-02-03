a8fe9e9dd8b5c0d1f6e5b65d5fbfd6eff00bf0e2
kalys01 <kalysosmon96.g@gmail.com>
2023-02-03 11:51:30 +0600

    Created a new reusable component for the main page.

---
+++ src/components/TheBlockComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="w-full flex justify-center"
+    :class="backgroundColor"
+  >
+    <div class="w-full flex flex-col items-center py-28 lg:w-11/12 max-w-screen-xl">
+    <h1 class="mb-[100px] text-4xl font-semibold">{{ title }}</h1>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: String,
+      required: false
+    },
+    backgroundColor: {
+      type: String,
+      required: false
+    }
+  })
+</script>

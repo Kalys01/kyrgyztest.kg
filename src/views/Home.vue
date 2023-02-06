ba6446d5e30c78a1e163a93abceff084a8a5899c
kalys01 <kalysosmon96.g@gmail.com>
2023-02-06 19:21:43 +0600

    Building the main page by blocks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,20 @@
 <template>
-  <TheCarousel />
+  <TheFirstMainBlock />
+  <!-- <TheCarousel /> -->
   <ThePayment />
   <TheStatistics />
+  <TheForSchool />
+  <TheGraphic />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import TheFirstMainBlock from "@/components/TheFirstMainBlock.vue";
 import TheCarousel from "@/components/TheCarousel.vue";
 import ThePayment from "@/components/Attention/ThePayment.vue";
 import TheStatistics from "@/components/Statistics/TheStatistics.vue";
+import TheForSchool from "@/components/TheForSchool.vue";
+import TheGraphic from "@/components/TheGraphic.vue";
 
-export default defineComponent({
-  name: "Home",
-  components: {
-    TheCarousel,
-    ThePayment,
-    TheStatistics,
-  },
-  setup() {},
-});
+name: "Home"
+
 </script>

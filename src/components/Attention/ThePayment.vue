defcb98c77269f53bb11b934c6a3b895644c7bc8
kalys01 <kalysosmon96.g@gmail.com>
2023-02-06 19:19:51 +0600

    Added a reused component. I will also break it into pieces.

--- src/components/Attention/ThePayment.vue
+++ src/components/Attention/ThePayment.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="flex flex-col lg:flex-row my-10 py-10 pl-5 lg:px-5 gap-5 bg-gray-300 dark:bg-gray-800">
-    <div class="flex flex-col gap-5">
-      <Attention />
-      <Ads />
-    </div>
-    <div class="flex flex-col justify-center lg:w-[700px] py-5 pl-10 pr-5 lg:px-5 rounded-l-[80px] lg:rounded-md  shadow-xl bg-[#673AB7] text-gray-300">
-      <h1 class="flex justify-center text-xl font-bold mb-2">ТЕСТ АКЫСЫ</h1>
-      <div class="flex flex-col gap-4 my-5 lg:my-0 text-sm md:text-md">
+  <TheBlockComponent
+    title="Тест акысы"
+    :backgroundColor="'bg-gray-100'"
+  >
+    <div class="w-full flex justify-end items-center relative">
+      <div class="flex">
+        <img src="@/assets/img/pay.jpg" class=" rounded-md" alt="pay">
+      </div>
+      <div class="flex flex-col absolute gap-5 p-7 left-12 text-sm md:text-md rounded-md backdrop-filter backdrop-blur-lg shadow-2xl">
         <div><span class="font-bold">Алуучу:</span><br />«Кыргызтест» Мамлекеттик мекемеси</div>
         <div><span class="font-bold">Алуучунун банкы:</span><br />КР борбордук казыналыгы</div>
         <div><span class="font-bold">Төлөм коду:</span><br />14235400</div>
@@ -17,10 +18,11 @@
         <div><span class="font-bold">Бир субтестти кайра тапшыруу баасы:</span><br />90 сом</div>
       </div>
     </div>
-  </div>
+  </TheBlockComponent>
 </template>
 
 <script lang="ts" setup>
+import TheBlockComponent from "@/components/TheBlockComponent.vue";
 import Attention from '@/components/Attention/Attention'
 import Ads from '@/components/Attention/Ads'
 </script>

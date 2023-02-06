c3f4afbf39139572ceb9d526308ef9d33db3642f
kalys01 <kalysosmon96.g@gmail.com>
2023-02-06 19:02:27 +0600

    For the first block of the main page. There will be more changes.

---
+++ src/components/TheFirstMainBlock.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full flex justify-center  bg-gray-900">
+      <div class="w-full flex items-center lg:w-11/12 max-w-screen-xl h-screen">
+      <div class="flex items-center ">
+        <h1 class="text-3xl font-semibold text-gray-300">«Кыргызтест» системасы боюнча жүргүзүлгөн бардык тесттер толук көлөмдө «Кыргызтест» Мамлекеттик мекемесинин сайтында онлайн көрсөтүлүп, видеого жазылып турат.</h1>
+      </div>
+      <div  class="overflow-hidden max-w-xl">
+        <img src="@/assets/img/mainPhoto.jpg" class="rounded-md" alt="first_main_block">  
+      </div>
+    </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>

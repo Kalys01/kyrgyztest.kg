18b9461908c19272f0cc76c712a2b305ad4b5334
kalys01 <kalysosmon96.g@gmail.com>
2023-02-06 19:28:53 +0600

    Prepared for new information

--- src/views/Forschool.vue
+++ src/views/Forschool.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="flex flex-col justify-center items-center py-20">
-    <h1 class="mb-[100px] text-4xl font-semibold">Выпускникам школ</h1>
-    <div class="flex flex-col justify-center items-center w-full max-w-[800px] px-3 mb-[180px] tracking-wider">
-      <div class="rounded-md overflow-hidden mb-16">
-        <img src="http://kyrgyztest.gov.kg/wp-content/uploads/2022/03/MG_0800_result-1-1280x720.jpg" alt="school">
-      </div>
-      <h2 class="text-2xl font-bold mb-10">Уважаемые тестируемые!</h2>
-      <div class="text-md">
-        <p class="mb-4 leading-7">
-          Согласно Постановлению Правительства Кыргызской Республики №279 от 11 июня 2018 года Государственное учреждение «Кыргызтест» тестирование по государственному языку для выпускников общеобразовательных учреждений будет проводиться с 20 апреля по 15 июля 2021 года.
-        </p>  
-        <h2 class="font-bold">При себе необходимо иметь:</h2>
-        <p class="flex flex-col pl-10 my-3 leading-8">
-          <span>документ, удостоверяющий личность (паспорт, свидетельство о рождении)</span>
-          <span>справку с места учебы, заверенную печатью и подписью.</span>
-        </p>
-        <h2 class="font-bold">Уровень для выпускников школ:</h2>
-        <p class="flex flex-col pl-10 my-3 leading-8">
-          <span>— с обучением на кыргызском языке – В1;</span>
-          <span>— с обучением на русском языке – А2.</span>
-          <span>Пересдача теста не допускается.</span>
-        </p>
-      </div>
+  <h1 class="mb-[100px] text-4xl font-semibold">Выпускникам школ</h1>
+  <div class="flex flex-col justify-center items-center w-full max-w-[800px] tracking-wider">
+    <div class="rounded-md overflow-hidden mb-16">
+      <img src="http://kyrgyztest.gov.kg/wp-content/uploads/2022/03/MG_0800_result-1-1280x720.jpg" alt="school">
+    </div>
+    <h2 class="text-2xl font-bold mb-10">Уважаемые тестируемые!</h2>
+    <div class="text-md">
+      <p class="mb-4 leading-7">
+        Согласно Постановлению Правительства Кыргызской Республики №279 от 11 июня 2018 года Государственное учреждение «Кыргызтест» тестирование по государственному языку для выпускников общеобразовательных учреждений будет проводиться с 20 апреля по 15 июля 2021 года.
+      </p>  
+      <h2 class="font-bold">При себе необходимо иметь:</h2>
+      <p class="flex flex-col pl-10 my-3 leading-8">
+        <span>документ, удостоверяющий личность (паспорт, свидетельство о рождении)</span>
+        <span>справку с места учебы, заверенную печатью и подписью.</span>
+      </p>
+      <h2 class="font-bold">Уровень для выпускников школ:</h2>
+      <p class="flex flex-col pl-10 my-3 leading-8">
+        <span>— с обучением на кыргызском языке – В1;</span>
+        <span>— с обучением на русском языке – А2.</span>
+        <span>Пересдача теста не допускается.</span>
+      </p>
     </div>
   </div>
 </template>
--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="">
-    <h1>This is an graph page</h1>
+  <h1 class="mb-[50px] md:mb-[100px] text-xl font-bold sm:text-4xl">Урматтуу тест тапшыруучулар!</h1>
+  <div class="flex flex-col justify-center w-full max-w-[800px] tracking-wider text-md leading-8">
+    <p class="mb-4 leading-7">
+      «Кыргызтест» мамлекеттик мекемеси саат <span class="font-bold">9:00</span>дон <span class="font-bold">18:00</span>го чейин иштейт, түшкү тыныгуу саат <span class="font-bold">12:00</span>дон <span class="font-bold">13:00</span>го чейин болот.
+    </p>
+    <h2 class="font-bold">Тест алдын ала бекитилген график боюнча эки нөөмөттө жүргүзүлөт.</h2>
+
+    <p class="flex flex-col pl-10 my-3 ">
+      <span>— Биринчи нөөмөткө каттоо саат 9:00до башталат</span>
+      <span>— Экинчи нөөмөткө каттоо саат 13:30да башталат</span>
+    </p>
+    <p>Тест акысы төлөнбөгөн учурда тестке катышууга уруксат берилбейт (КР 2014-жылдын 17-июлундагы №139 «Мамлекеттик жана муниципалдык кызмат көрсөтүүлөр жөнүндө» Мыйзамынын 2-главасынын 7-беренесинин 1, 2-пункттарынан тышкары)</p>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 
-export default defineComponent({
-
-})
 </script>
 

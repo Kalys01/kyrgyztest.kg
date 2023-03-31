dd4712ae5a7c91c6e819de4c3055fca1e08e6e2f
kalys01 <kalysosmon96.g@gmail.com>
2023-03-31 17:46:17 +0600

    Adapted this component for the mobile phone

--- src/components/MainPageBlocks/Accordion/TheAccordion.vue
+++ src/components/MainPageBlocks/Accordion/TheAccordion.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class="w-full text-md md:text-xl font-bold py-4 leading-relaxed border-t-[.5px] border-gray-400 dark:text-gray-100">
+    <div class="w-full text-md md:text-xl font-semibold py-4 leading-relaxed border-t-[.5px] border-gray-400 dark:text-gray-100">
       <div
         class="flex justify-between cursor-pointer"
         @click="isOpen = !isOpen"
       >
         <h1>{{title}}</h1>
-        <PlusIcon
-          class="h-6 w-6 transform transition duration-300"
-          :class="isOpen&&'rotate-45'"
-        />
+        <div class="flex justify-center items-center ml-2">
+          <PlusIcon
+            class="h-6 w-6 transform transition duration-300fw"
+            :class="isOpen&&'rotate-45'"
+          />
+        </div>
       </div>
 
       <p
-        class="text-sm md:text-md text-justify p-1 pr-8 font-mono leading-6 text-gray-600"
+        class="text-sm md:text-md text-justify px-1 my-3 font-mono leading-6 text-gray-600"
         :class="isOpen ? 'd-block' : 'hidden'"
       >
         <slot></slot>

1b8326899ba1a299429d4fe309f16e4c02206755
kalys01 <kalysosmon96.g@gmail.com>
2023-04-05 19:14:43 +0600

    Adapted for mobile phone

--- src/components/MainPageBlocks/TheFirstMainBlock.vue
+++ src/components/MainPageBlocks/TheFirstMainBlock.vue
@@ -1,13 +1,14 @@
 <template>
-    <div class="w-full flex justify-center  bg-gray-900">
-      <div class="w-full flex items-center lg:w-11/12 max-w-screen-xl h-screen">
-      <div class="flex items-center ">
-        <h1 class="text-3xl font-semibold text-gray-300">«Кыргызтест» системасы боюнча жүргүзүлгөн бардык тесттер толук көлөмдө «Кыргызтест» Мамлекеттик мекемесинин сайтында онлайн көрсөтүлүп, видеого жазылып турат.</h1>
+    <div class="w-full flex justify-center bg-gray-100 md:bg-gray-900">
+      <div class="relative overflow-hidden max-w-screen-xl flex h-[700px] rounded-b-3xl md:rounded-none">
+        <div class="">
+          <img src="@/assets/img/mainPhoto.jpg" class="object-none h-full lg:h-auto lg:w-full" alt="first_main_block">  
+        </div>
+        <div class="absolute w-full h-full bg-gradient-to-r from-gray-900"></div>
+        <div class="absolute flex items-center bottom-0 md:bottom-auto md:top-[200px] w-full md:w-3/4 p-[20px] bg-black/20 backdrop-filter backdrop-blur-sm rounded-b-3xl md:rounded-none">
+          <h1 class="text-xl sm:text-2xl font-semibold text-gray-300">«Кыргызтест» системасы боюнча жүргүзүлгөн бардык тесттер толук көлөмдө «Кыргызтест» Мамлекеттик мекемесинин сайтында онлайн көрсөтүлүп, видеого жазылып турат.</h1>
+        </div>
       </div>
-      <div  class="overflow-hidden max-w-xl">
-        <img src="@/assets/img/mainPhoto.jpg" class="rounded-md" alt="first_main_block">  
-      </div>
-    </div>
     </div>
 </template>
 

ed07ccf2eef4e1c7c0175e1e812bf4935d0b9f44
kalys01 <kalysosmon96.g@gmail.com>
2023-07-20 14:31:50 +0600

    Changed the icon added the sun icon and moon icon

--- src/components/TheToggleTheme.vue
+++ src/components/TheToggleTheme.vue
@@ -3,30 +3,30 @@
     @click="toggleTheme"
     class="text-gray-300 cursor-pointer h-5 w-5"
   >
-    <svg
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="fas"
-      data-icon="adjust"
-      class="svg-inline--fa fa-adjust fa-w-16"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 512 512"
-    >
-      <path
-        fill="currentColor"
-        d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z"
-      />
-    </svg>
+    <SunIcon
+      v-if="isDayNigth"
+      class="h-6 w-6 text-yellow-400"
+    />
+    <MoonIcon
+      v-else
+      class="h-6 w-6 text-gray-300"
+    />
   </button>
 </template>
 
 <script lang="ts" setup>
 import { ActionTypes } from "@/store/modules/action-types";
 import { useStore } from "vuex";
+import { ref, computed } from "vue";
+import { SunIcon, MoonIcon } from '@heroicons/vue/solid';
 
 const store = useStore();
 
-const toggleTheme = () => store.dispatch(ActionTypes.toggle_theme);
+const isDayNigth = ref(store.state.theme === 'dark');
+
+const toggleTheme = () => {
+  isDayNigth.value = !isDayNigth.value;
+  store.dispatch(ActionTypes.toggle_theme);
+}
 
 </script>

2d143bf801e92a94ca4a7cf517a1610cdbf32b3e
kalys01 <kalysosmon96.g@gmail.com>
2023-11-01 11:32:40 +0600

    Created a new folder for the news, because there are a lot of components there and so that components do not clog up

---
+++ src/components/News/NewsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="relative grid w-[92%] sm:w-[94%] h-80 sm:h-52 md:h-40 lg:h-48 mt-9 md:mt-0 xl:h-56 max-h-80 mx-3 xs:mx-5 md:mx-10 rounded-md overflow-hidden bg-black cursor-pointer shadow-2xl">
+    <div class="w-full h-full transition duration-300 ease-in-out text-gray-400 absolute z-10 hover:text-white">
+      <div class="absolute w-full h-[70%] sm:h-full bottom-0 p-3 sm:p-4 md:p-6 sm:w-[80%] rounded-md bg-opacity-50 sm:bg-opacity-0 backdrop-filter sm:backdrop-filter-none backdrop-blur-lg sm:backdrop-blur-none">
+        <h1 class="timeline-title text-md lg:text-xl xl:text-3xl">{{ post.title }}</h1>
+        <p class="text-gray-300 text-xs lg:text-sm ">
+          {{ truncateText(post.body, 333) }}
+        </p>
+      </div>
+    </div>
+    <div class="overflow-hidden w-full h-1/2 sm:h-full sm:w-1/2 z-0 absolute sm:right-0">
+      <img class="object-cover w-full sm:h-full" src="http://kyrgyztest.gov.kg/wp-content/uploads/2022/01/ОШМУ_1-520x245.jpeg" alt="">
+    </div>
+    <div class="absolute w-full sm:w-1/2 h-1/2 sm:h-full sm:right-0 bg-gradient-to-t from-black sm:bg-gradient-to-r sm:from-black" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useStore } from "@/store";
+
+const props = defineProps({
+    post: {
+      type: Object,
+      required: true
+    }
+  });
+
+const store = useStore();
+
+function truncateText(text: string, maxLength: number): string {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + "...";
+  } else {
+    return text;
+  }
+};
+
+</script>
---
+++ src/components/News/TheNews.vue
@@ -0,0 +1,92 @@
+<template>
+  <div
+    class="grid grid-cols-12 bg-gray-500 "
+    v-if="isLoading"
+    v-for="i in 5"
+  >
+    <section class="col-span-1 md:col-span-3 bg-[#212121]" />
+    <section class="col-span-11 md:col-span-9 border-l-2  py-5 border-gray-300 bg-gray-100 dark:bg-[#212121] transition duration-500">
+      <SkeletonLoader class=" grid w-[92%] sm:w-[94%] h-80 sm:h-52 md:h-40 lg:h-48 mt-9 md:mt-0 xl:h-56 max-h-80 mx-3 xs:mx-5 md:mx-10 rounded-md overflow-hidden shadow-2xl" />
+    </section>
+  </div>
+
+  <div
+    class="grid grid-cols-12"
+    v-else
+    v-for="post in paginatedPosts"
+    :key="post.id"
+  >
+    <section class="col-span-1 md:col-span-3 bg-[#212121]"/>
+
+    <section class="col-span-11 md:col-span-9 border-l-2  py-5 border-gray-300 bg-gray-100 dark:bg-[#212121] transition duration-500">
+      <div class="sticky top-0 z-20 bg-white w-full py-5 hidden">
+        <span>2022</span>
+      </div>
+      <div>
+      </div>
+      <div class="py-5">
+        <div class="w-5 h-5 rounded-full absolute -ml-3 bg-[#673AB7] flex justify-center items-center border-2 border-white">
+          <div class="absolute">
+            <ClockIcon class="h-6 w-6 text-gray-300"/>
+          </div>
+          <span class="absolute ml-36 w-[100px] md:-ml-36 text-gray-400 md:text-gray-300 text-xs md:text-sm dark:text-gray-300  transition duration-500">13 Feb 2022</span>
+        </div>
+        <router-link :to="`/news/${post.id}`">
+          <NewsList :post="post"/>
+        </router-link>
+      </div>
+    </section>
+  </div>
+
+  <v-pagination
+    class="flex justify-center text-gray-900 dark:text-gray-300"
+    v-model="currentPage"
+    :pages="totalPages"
+    :range-size="1"
+    active-color="#673AB7"
+    @update:modelValue="changePage(currentPage)"
+  />
+
+</template>
+
+<script lang="ts" setup>
+import { computed, ref, onMounted } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import NewsList from "@/components/News/NewsList.vue";
+import SkeletonLoader from "@/components/UI/SkeletonLoader.vue";
+import { ClockIcon } from "@heroicons/vue/solid";
+import { useStore } from "@/store";
+import { ActionTypes } from "@/store/modules/action-types";
+import VPagination from "@hennge/vue3-pagination";
+import "@hennge/vue3-pagination/dist/vue3-pagination.css";
+
+const router = useRouter();
+const route = useRoute();
+const store = useStore();
+
+const currentPage = ref(Number(route.query.page) || 1);
+
+const itemsPerPage = 7; // Количество новостей на странице
+
+onMounted( () => store.dispatch(ActionTypes.FetchPosts) );
+
+const isLoading = computed( () => store.getters.GET_LOADING );
+
+const posts = computed( () => store.getters.GET_POST );
+
+const totalPages = computed( () => Math.ceil(posts.value.length / itemsPerPage) );
+
+const paginatedPosts = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  return posts.value.slice(startIndex, endIndex);
+});
+
+function changePage(page: number) {
+  router.push(`${route.path}?page=${page}`)  // saves current page when reloading 
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page;
+  }
+};
+
+</script>

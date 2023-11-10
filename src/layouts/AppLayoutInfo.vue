5136edbd6b126f8d342c9f7365aa57be501a031c
kalys01 <kalysosmon96.g@gmail.com>
2023-11-10 19:49:28 +0600

    Added width for skeleton

--- src/layouts/AppLayoutInfo.vue
+++ src/layouts/AppLayoutInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex justify-center bg-white dark:bg-gray-900 dark:text-white transition duration-500">
-    <div class="flex flex-col items-center lg:w-11/12 max-w-screen-xl mt-20 min-h-screen mx-3 mb-[200px]">
+    <div class="flex flex-col items-center w-full lg:w-11/12 max-w-screen-xl mt-20 min-h-screen mx-3 mb-[200px]">
       <slot />
     </div>
   </div>

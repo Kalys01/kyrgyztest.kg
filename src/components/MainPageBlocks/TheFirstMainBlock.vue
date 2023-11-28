43a253a1b1e1241ad540a7115f6b3ab20593f5d2
kalys01 <kalysosmon96.g@gmail.com>
2023-11-28 20:25:40 +0600

    I did everything via json translation

--- src/components/MainPageBlocks/TheFirstMainBlock.vue
+++ src/components/MainPageBlocks/TheFirstMainBlock.vue
@@ -6,7 +6,7 @@
         </div>
         <div class="absolute w-full h-full bg-gradient-to-r from-gray-900"></div>
         <div class="absolute flex items-center bottom-0 md:bottom-auto md:top-[200px] w-full md:w-3/4 p-[20px] bg-black/20 backdrop-filter backdrop-blur-sm rounded-b-3xl md:rounded-none">
-          <h1 class="text-xl sm:text-2xl font-semibold text-gray-300">«Кыргызтест» системасы боюнча жүргүзүлгөн бардык тесттер толук көлөмдө «Кыргызтест» Мамлекеттик мекемесинин сайтында онлайн көрсөтүлүп, видеого жазылып турат.</h1>
+          <h1 class="text-xl sm:text-2xl font-semibold text-gray-300">{{$t('mainPage.welcome')}}</h1>
         </div>
       </div>
     </div>

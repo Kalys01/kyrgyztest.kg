51d51858f92ce4e32c738832a41104626525fb8d
kalys01 <kalysosmon96.g@gmail.com>
2024-01-08 20:21:21 +0600

    Created a new folder for the course file

---
+++ src/components/Courses/TheCourse.vue
@@ -0,0 +1,97 @@
+<template>
+<TheBlockComponent
+  :title="$t('сourses.title')"
+>
+  <div class="w-full grid gap-8 grid-cols-[repeat(auto-fill,minmax(350px,2fr))]">
+    <div class="bg-image bg-contain rounded-md overflow-hidden h-72 bottom-6 shadow-xl drop-shadow-2xl relative">
+    <div class="backdrop-blur-md bg-contain absolute bottom-0 left-0 h-full w-full"
+    style="background-image: inherit; -webkit-mask-image: linear-gradient(0deg, transparent, transparent 0%, transparent 30%, black 70%);"
+    >
+    <img src="../../assets/img/typescript-with-vue-js-3.jpg" class="w-full">
+    </div>
+
+    <div class="absolute bottom-0 left-0 w-full p-4">
+
+      <p class="font-bold ">Vue 3 Single File Components</p>
+      <p class="text-gray-700 text-xs">Embrace the full power of JavaScript bundlers and Vue.js by learning how to create and manage Single File Components.</p>
+    </div>
+  </div>
+
+  <div class="bg-image bg-contain rounded-md overflow-hidden  bottom-6 h-72 shadow-xl drop-shadow-2xl relative">
+    <div class="backdrop-blur-md bg-contain absolute bottom-0 left-0 h-full w-full"
+    style="background-image: inherit; -webkit-mask-image: linear-gradient(0deg, transparent, transparent 0%, transparent 30%, black 70%);"
+    >
+    <img src="../../assets/img/typescript-with-vue-js-3.jpg" class="w-full">
+    </div>
+
+    <div class="absolute bottom-0 left-0 w-full p-4">
+
+      <p class="font-bold ">Vue 3 Single File Components</p>
+      <p class="text-gray-700 text-xs">Embrace the full power of JavaScript bundlers and Vue.js by learning how to create and manage Single File Components.</p>
+    </div>
+  </div>
+
+  <div class="bg-image bg-contain rounded-md overflow-hidden bottom-6 h-72 shadow-xl drop-shadow-2xl relative">
+    <div class="backdrop-blur-md bg-contain absolute bottom-0 left-0 h-full w-full"
+    style="background-image: inherit; -webkit-mask-image: linear-gradient(0deg, transparent, transparent 0%, transparent 30%, black 70%);"
+    >
+    <img src="../../assets/img/typescript-with-vue-js-3.jpg" class="w-full">
+    </div>
+
+    <div class="absolute bottom-0 left-0 w-full p-4">
+
+      <p class="font-bold ">Vue 3 Single File Components</p>
+      <p class="text-gray-700 text-xs">Embrace the full power of JavaScript bundlers and Vue.js by learning how to create and manage Single File Components.</p>
+    </div>
+  </div>
+
+  <div class="bg-image bg-contain rounded-md overflow-hidden bottom-6 h-72 shadow-xl drop-shadow-2xl relative">
+    <div class="backdrop-blur-md bg-contain absolute bottom-0 left-0 h-full w-full"
+    style="background-image: inherit; -webkit-mask-image: linear-gradient(0deg, transparent, transparent 0%, transparent 30%, black 70%);"
+    >
+    <img src="../../assets/img/typescript-with-vue-js-3.jpg" class="w-full">
+    </div>
+
+    <div class="absolute bottom-0 left-0 w-full p-4">
+
+      <p class="font-bold ">Vue 3 Single File Components</p>
+      <p class="text-gray-700 text-xs">Embrace the full power of JavaScript bundlers and Vue.js by learning how to create and manage Single File Components.</p>
+    </div>
+  </div>
+
+  <div class="bg-image bg-contain rounded-md overflow-hidden bottom-6 h-72 shadow-xl drop-shadow-2xl relative">
+    <div class="backdrop-blur-md bg-contain absolute bottom-0 left-0 h-full w-full"
+    style="background-image: inherit; -webkit-mask-image: linear-gradient(0deg, transparent, transparent 0%, transparent 30%, black 70%);"
+    >
+    <img src="../../assets/img/typescript-with-vue-js-3.jpg" class="w-full">
+    </div>
+
+    <div class="absolute bottom-0 left-0 w-full p-4">
+
+      <p class="font-bold ">Vue 3 Single File Components</p>
+      <p class="text-gray-700 text-xs">Embrace the full power of JavaScript bundlers and Vue.js by learning how to create and manage Single File Components.</p>
+    </div>
+  </div>
+
+  <div class="bg-image bg-contain rounded-md overflow-hidden bottom-6 h-72 shadow-xl drop-shadow-2xl relative">
+    <div class="backdrop-blur-md bg-contain absolute bottom-0 left-0 h-full w-full"
+    style="background-image: inherit; -webkit-mask-image: linear-gradient(0deg, transparent, transparent 0%, transparent 30%, black 70%);"
+    >
+    <img src="../../assets/img/typescript-with-vue-js-3.jpg" class="w-full">
+    </div>
+
+    <div class="absolute bottom-0 left-0 w-full p-4">
+
+      <p class="font-bold ">Vue 3 Single File Components</p>
+      <p class="text-gray-700 text-xs">Embrace the full power of JavaScript bundlers and Vue.js by learning how to create and manage Single File Components.</p>
+    </div>
+  </div>
+  </div>
+  
+</TheBlockComponent>
+</template>
+
+<script lang="ts" setup>
+import TheBlockComponent from "@/components/TheBlockComponent.vue";
+
+</script>

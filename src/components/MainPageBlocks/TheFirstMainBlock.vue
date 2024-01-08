d0ed2813025e9266b4b9f8080cde02c948461d0c
kalys01 <kalysosmon96.g@gmail.com>
2024-01-08 20:44:42 +0600

    Changed the name of the greeting

--- src/components/MainPageBlocks/TheFirstMainBlock.vue
+++ src/components/MainPageBlocks/TheFirstMainBlock.vue
@@ -5,8 +5,8 @@
           <img src="@/assets/img/mainPhoto.jpg" class="object-none h-full lg:h-auto lg:w-full" alt="first_main_block">  
         </div>
         <div class="absolute w-full h-full bg-gradient-to-r from-gray-900"></div>
-        <div class="absolute flex items-center bottom-0 md:bottom-auto md:top-[200px] w-full md:w-3/4 p-[20px] bg-black/20 backdrop-filter backdrop-blur-sm rounded-b-3xl md:rounded-none">
-          <h1 class="text-xl sm:text-2xl font-semibold text-gray-300">{{$t('mainPage.welcome')}}</h1>
+        <div class="absolute flex items-center bottom-0 md:bottom-auto md:top-[400px] w-full md:w-auto p-[20px] bg-black/20 backdrop-filter backdrop-blur-sm rounded-b-3xl md:rounded-none">
+          <h1 class="text-xl sm:text-4xl font-semibold text-gray-300">{{$t('mainPage.welcome')}}</h1>
         </div>
       </div>
     </div>
